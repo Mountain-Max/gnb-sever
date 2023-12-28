8f966ecdbec295b790a892ec670376f9c9a29b1b
Max Kolbin <kolbinmaxim013@gmail.com>
2023-12-28 18:16:50 +0200

    gnb-sever

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// todo Carousel
+$(document).ready(function () {
+  const photoCarousel = $("#photo-carousel");
+
+  photoCarousel.owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+    navText: ["", ""],
+  });
+
+  // находим кнопки назад и вперед
+  const sliderLeft = $('.slider-button-left');
+  const sliderRight = $('.slider-button-right');
+
+  sliderLeft.click(function () {
+    photoCarousel.trigger('prev.owl.carousel')
+  });
+
+  sliderRight.click(function () {
+    photoCarousel.trigger('next.owl.carousel')
+  });
+
+});
+// todo Carousel //
+
+
+// todo Navigation
+// Скрипт для навигации
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.nav');
+
+navIcon.addEventListener('click', function () {
+  this.classList.toggle('nav-icon--active');
+  nav.classList.toggle('nav--active');
+});
+
+// Находим ссылки внутри мобильной навигации
+const navLinks = document.querySelectorAll('.nav a');
+
+// Обходим ссылки методом forEach
+navLinks.forEach(function (item) {
+  // Для каждой ссылки добавляем прослушку по событию "Клик"
+  item.addEventListener('click', function () {
+    navIcon.classList.remove('nav-icon--active'); // Убираем активный класс у иконки моб. навигации
+    nav.classList.remove('nav--active'); // Убираем активный класс у блока моб. навигации
+  })
+})
+
+// todo Navigation //
